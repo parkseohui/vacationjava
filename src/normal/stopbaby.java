5c762a6b222ac782abd23ac340f2c17e01bbbcf0
Unknown <skfhddl003@gmail.com>
2018-08-01 18:58:06 +0900

    반복

---
+++ src/normal/stopbaby.java
@@ -0,0 +1,15 @@
+package normal;
+import java.util.Scanner;
+public class stopbaby {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+	
+		int i=1;
+		for(i=1;i<20;i++) {
+			System.out.println(i+" "+"Á¤·Ä~!");
+			if(i==7) break;
+		}
+System.out.println();
+}}
