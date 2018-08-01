9f379fb247a9cb94562b5b9dc6d898785dfe3cf0
Unknown <skfhddl003@gmail.com>
2018-08-01 14:08:37 +0900

    스코어
    스코어!!!

---
+++ src/normal/scoreifelse.java
@@ -0,0 +1,12 @@
+package normal;
+
+public class scoreifelse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int score=65;
+if(60<=score)
+	System.out.println("합격입니다.");
+else
+	System.out.println("불합격입니다.");
+	}}
