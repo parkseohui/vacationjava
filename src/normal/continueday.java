7360eb6d0d18b2cd6430c0e60b0fdfad5193a46b
Unknown <skfhddl003@gmail.com>
2018-08-02 09:31:46 +0900

    continue문

---
+++ src/normal/continueday.java
@@ -0,0 +1,14 @@
+package normal;
+
+public class continueday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=1;
+for(i=1;i<=10;i++) {
+	if(i%3==0)continue;
+	System.out.println(i+" ");
+}System.out.println("");
+	}
+
+}
