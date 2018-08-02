9d9a39f00125bce835133311ffb745e9f98a904e
Unknown <skfhddl003@gmail.com>
2018-08-02 09:35:35 +0900

    배열선언

---
+++ src/normal/arraydeclaration.java
@@ -0,0 +1,22 @@
+package normal;
+
+public class arraydeclaration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] month;
+		double[] values;
+
+		month = new int[4];
+		values = new double[3];
+		System.out.println(month.length);
+		System.out.println(values.length);
+
+		char[] ch = new char[6];
+		float[] f = new float[5];
+		f = new float[7];
+		System.out.println(ch.length);
+		System.out.println(f.length);
+	}
+
+}
